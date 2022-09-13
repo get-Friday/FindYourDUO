f0feeebb29c3567f78075da8c81ab5f9864144ea
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-13 16:18:32 -0300

    Changes structure for better wrapping

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,9 +3,12 @@ import logoImg from './assets/Logo.svg'
 
 function App() {
   return (
-    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
-      <img src={logoImg} />
-    </div>
+    <main className='max-w-[1344px] mx-auto flex flex-col items-center'>
+      <div className='my-20'>
+        <img src={logoImg} />
+      </div>
+      <h1 className='text-6xl text-white font-black'>Seu duo está aqui.</h1>
+    </main>
   )
 }
 
