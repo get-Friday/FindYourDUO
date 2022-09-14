314c99703a7a68398c75a705cf3d39bf1898be9d
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-14 17:10:54 -0300

    Server: Implements get /games

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,10 @@ import { PrismaClient } from '@prisma/client'
 const app = express()
 const prisma = new PrismaClient()
 
-app.get('/games', (req, res) => res.json([]))
+app.get('/games', async (req, res) => {
+    const games = await prisma.game.findMany()
+    return res.json(games)
+})
 
 app.post('/ads', (req, res) => res.status(201).json([]))
 
