044240628e5abb791f507b1c67b364d667be16b7
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-14 10:55:12 -0300

    Mobile: Implements GameCard on home screen

--- mobile/src/screens/home/index.tsx
+++ mobile/src/screens/home/index.tsx
@@ -2,6 +2,8 @@ import { Image, View } from 'react-native'
 import { styles } from './styles'
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from '../../components/heading'
+import { GameCard } from '../../components/gameCard'
+import { GAMES } from '../../utils/games'
 
 export function Home(){
     return (
@@ -13,6 +15,9 @@ export function Home(){
             <Heading 
                 title='Encontre seu duo!'
                 subtitle='Selecione o game que deseja jogar...'
+            />
+            <GameCard 
+                data={GAMES[0]}
             />
         </View>
     )
