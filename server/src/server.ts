1eede6d77e8045d36471c49abd7783372169279b
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-14 17:32:30 -0300

    Server: Implements ads/id/discord

--- server/src/server.ts
+++ server/src/server.ts
@@ -46,6 +46,17 @@ app.get('/games/:id/ads', async (req, res) => {
     }))
 })
 
-app.get('/ads/:id/discord', (req, res) => res.json([]))
+app.get('/ads/:id/discord', async (req, res) => {
+    const adId = req.params.id
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true
+        },
+        where: {
+            id: adId
+        }
+    })
+    return res.json(ad)
+})
 
 app.listen(3333)
