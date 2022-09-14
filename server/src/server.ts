13278ac3a063db77dc3ee11454ab91b0af734379
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-14 16:25:40 -0300

    Server: Define routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,12 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (req, res) => res.json())
+app.get('/games', (req, res) => res.json([]))
+
+app.post('/ads', (req, res) => res.status(201).json([]))
+
+app.get('/games/:id/ads', (req, res) => res.json([]))
+
+app.get('/ads/:id/discord', (req, res) => res.json([]))
 
 app.listen(3333)
