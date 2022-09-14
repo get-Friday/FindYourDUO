d29d35e2010fe4077847d5ed47c8ab4d4d99d837
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-14 10:25:03 -0300

    Mobile: Implements fonts and loading screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,22 @@
 import { StatusBar } from 'react-native'
+import { 
+  useFonts, 
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_700Bold, 
+  Inter_900Black 
+} from '@expo-google-fonts/inter'
 import { Background } from './src/components/background'
+import { Home } from './src/screens/home'
+import { Loading } from './src/components/loading'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black 
+  })
   return (
     <Background>
       <StatusBar 
@@ -9,6 +24,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
+      { fontsLoaded ? <Home /> : <Loading /> }
     </Background>
   );
 }
