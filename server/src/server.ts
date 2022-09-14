ebae0a78a6080c070f6e22df085ff03098f859ad
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-14 17:18:28 -0300

    Server: Include on get /games ads counter

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,15 @@ const app = express()
 const prisma = new PrismaClient()
 
 app.get('/games', async (req, res) => {
-    const games = await prisma.game.findMany()
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true
+                }
+            }
+        }
+    })
     return res.json(games)
 })
 
