d7e02743d4060dd1de827c9e66c2407b7ef92f51
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-15 10:50:13 -0300

    Web feat: Implements frontend communication with backend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,27 @@ import './styles/main.css'
 import logoImg from './assets/Logo.svg'
 import { GameBanner } from './components/GameBanner'
 import { CreateAdBanner } from './components/CreateAdBanner'
+import { useState, useEffect } from 'react'
+
+interface Game {
+  id: string
+  title: string
+  bannerUrl: string
+  _count: {
+    ads: number;
+  }
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([])
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(res => res.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, [])
+
   return (
     <main className='max-w-[1344px] mx-auto flex flex-col items-center'>
       <div className='my-20'>
@@ -13,12 +32,16 @@ function App() {
         Seu <span className='bg-nlw-gradient text-transparent bg-clip-text'>duo</span> está aqui.
       </h1>
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameBanner bannerUrl='/image1.png' title='League of Legends' adsCount={4}/>
-        <GameBanner bannerUrl='/image2.png' title='Dota 2' adsCount={4}/>
-        <GameBanner bannerUrl='/image3.png' title='CS:GO' adsCount={7}/>
-        <GameBanner bannerUrl='/image4.png' title='Apex Legends' adsCount={4}/>
-        <GameBanner bannerUrl='/image5.png' title='Fortnite' adsCount={2}/>
-        <GameBanner bannerUrl='/image6.png' title='World of Warcraft' adsCount={11}/>
+        {games.map(game => {
+          return (
+            <GameBanner 
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          )
+        })}
       </div>
       <CreateAdBanner />
     </main>
