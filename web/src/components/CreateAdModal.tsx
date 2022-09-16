45d92ced5eaaf18f39297570a951356030ddc483
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-16 19:51:16 -0300

    Web feat: implements checkbox with radix for create ad modal

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { GameController } from 'phosphor-react'
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { Check, GameController } from 'phosphor-react'
 import { Input } from './Form/Input'
 
 export function CreateAdModal() {
@@ -53,8 +54,12 @@ export function CreateAdModal() {
             </div>
         </div>
 
-        <div className='mt-2 flex gap-2 text-sm'>
-          <Input type="checkbox" id="useVoice" />
+        <div className='mt-2 flex items-center gap-2 text-sm'>
+        <Checkbox.Root className='w-6 h-6 p-1 rounded bg-zinc-900'>
+          <Checkbox.Indicator>
+            <Check className='w-4 h-4 text-emerald-400' />
+          </Checkbox.Indicator>
+        </Checkbox.Root>
           Costumo me conectar ao chat de voz
         </div>
 
