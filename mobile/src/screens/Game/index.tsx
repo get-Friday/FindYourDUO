2f5ab71130ccbf0fbb504b79d9cca6663c474880
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-15 22:54:22 -0300

    Mobile feat: implements header on game screen

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,13 +1,33 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useRoute } from '@react-navigation/native'
 import { Background } from '../../components/Background'
 import { styles } from './styles'
+import { GameParams } from '../../@types/navigation'
+import { View, TouchableOpacity, Image } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+import { THEME } from '../../theme'
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading'
 
 export function Game() {
-    return (
-        <Background>
-            <SafeAreaView style={styles.container}>
-
-            </SafeAreaView>
-        </Background>
-    )
+  const route = useRoute()
+  const game = route.params as GameParams
+  return (
+  <Background>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Entypo 
+            name='chevron-thin-left' 
+            color={THEME.COLORS.CAPTION_300}
+            size={20}
+          />
+        </TouchableOpacity>
+        <Image source={logoImg} style={styles.logo} />
+        <View style={styles.right}/>
+      </View>
+      <Heading title={game.title} subtitle='Conecte-se e começe a jogar!'/>
+    </SafeAreaView>
+  </Background>
+  )
 }
