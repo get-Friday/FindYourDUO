df941931fb98032be36fa007d6a80fd06a471662
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-15 23:39:13 -0300

    Mobile feat: DuoCard styles component to show ads from games

---
+++ mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,36 @@
+import { TouchableOpacity, View } from 'react-native'
+import { THEME } from '../../theme'
+import { DuoInfo } from '../DuoInfo'
+import { styles } from './styles'
+
+export interface DuoCardProps {
+    id: string
+    name: string
+    useVoiceChannel: boolean
+    weekDays: string[]
+    yearsPlaying: number
+    hourEnd: string
+    hourStart: string
+}
+
+interface Props {
+    data: DuoCardProps
+}
+
+export function DuoCard({ data }: Props) {
+    return (
+        <View style={styles.container}>
+            <DuoInfo label='Nome' value={data.name}/>
+            <DuoInfo label='Tempo de jogo' value={`${data.yearsPlaying} ano(s)`}/>
+            <DuoInfo label='Disponibilidade' value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}/>
+            <DuoInfo 
+                label='Chamada de áudio?' 
+                value={data.useVoiceChannel ? 'Sim' : 'Não'} 
+                colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
+            />
+            <TouchableOpacity style={styles.button}>
+            
+            </TouchableOpacity>
+        </View>
+    )
+}
