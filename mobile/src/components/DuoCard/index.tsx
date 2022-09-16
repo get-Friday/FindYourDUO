43959006ba5ad5eba0a3e91179bfb083eba44430
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-15 23:55:50 -0300

    Mobile feat: Sets up on DuoCard button to recieve discord

--- mobile/src/components/DuoCard/index.tsx
+++ mobile/src/components/DuoCard/index.tsx
@@ -16,9 +16,10 @@ export interface DuoCardProps {
 
 interface Props {
     data: DuoCardProps
+    onConnect: () => void
 }
 
-export function DuoCard({ data }: Props) {
+export function DuoCard({ data, onConnect }: Props) {
     return (
         <View style={styles.container}>
             <DuoInfo label='Nome' value={data.name}/>
@@ -29,7 +30,7 @@ export function DuoCard({ data }: Props) {
                 value={data.useVoiceChannel ? 'Sim' : 'Não'} 
                 colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
             />
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={onConnect}>
                 <GameController color={THEME.COLORS.TEXT} size={20}/>
                 <Text style={styles.buttonTitle}>Conectar</Text>
             </TouchableOpacity>
