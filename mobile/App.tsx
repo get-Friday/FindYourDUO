59dce5dfc0a5040978612c64b18e6175eeed56b3
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-09-17 10:19:05 -0300

    Mobile feat: starts implementing push notification on App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react'
 import { StatusBar } from 'react-native'
 import { 
   useFonts, 
@@ -6,17 +7,49 @@ import {
   Inter_700Bold, 
   Inter_900Black 
 } from '@expo-google-fonts/inter'
+import { Subscription } from 'expo-modules-core'
+import * as Notifications from 'expo-notifications'
+
 import { Background } from './src/components/Background'
 import { Loading } from './src/components/Loading'
 import { Routes } from './src/routes'
 
+import './src/service/notificationConfigs'
+import { getPushNotificationToken } from './src/service/getPushNotificationToken'
+
+
 export default function App() {
+  const getNotificationListener = useRef<Subscription>()
+  const responseNotificationListener = useRef<Subscription>()
   const [fontsLoaded] = useFonts({
     Inter_400Regular, 
     Inter_600SemiBold, 
     Inter_700Bold, 
     Inter_900Black 
   })
+  
+  useEffect(() => {
+    getPushNotificationToken()
+  })
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications
+      .addNotificationReceivedListener(notification => {
+        console.log(notification)
+      })
+
+    responseNotificationListener.current = Notifications
+      .addNotificationResponseReceivedListener(response => {
+        console.log(response)
+      })
+
+    return () => {
+      if(getNotificationListener.current && responseNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current)
+        Notifications.removeNotificationSubscription(responseNotificationListener.current)
+      }
+    }
+  }, [])
   return (
     <Background>
       <StatusBar 
